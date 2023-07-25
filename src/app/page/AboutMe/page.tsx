8a07c62039b1c7e084e7fdac99a8ed599d0b5897
Wesley Morais de Oliveira <f0fp1252@t-systems.com.br>
2023-07-25 14:34:05 -0300

    botão aboutMe

--- src/app/page/AboutMe/page.tsx
+++ src/app/page/AboutMe/page.tsx
@@ -1,16 +1,18 @@
+
 export default function AboutMe() {
     return(
-        <div className="flex">
-            <div className="flex flex-col align-center justify-center">
+        <div className="flex align-item justify-center pt-72 gap-10">
+             <div>
+             <img src="./img/Avatar-Maker.png" alt="imagem cartoon" width="400" height="400"/>
+            </div>
+            <div className="flex flex-col align-center justify-center gap-1">
                 <h1 className="text-4xl">Yago Santana Rocha</h1>
                 <p>Olá sou o yago Tenho 19 anos</p>
                 <p>Desenvolvedor Front-end</p>
                 <p>Analise e desenvolvimento de sistemas 4 Semestre</p>
-                <p>Interesses em ReactJS, NextJS, VueJs</p>
+                <p>Interesses em ReactJS, NextJS, JavaScript</p>
             </div>
-            <div>
-                <img src="./img/foto-yago.jpg" alt="" />
-            </div>
+           
         </div>
         
     )
