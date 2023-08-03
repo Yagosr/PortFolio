c4e6c76ac0df21d459bed1c7ce12c6d2243fb286
Yagom <yagosr25@gmail.com>
2023-08-03 16:19:50 -0300

    about me

---
+++ src/app/components/ButtonBack/page.tsx
@@ -0,0 +1,7 @@
+export default function ButtonBack() {
+    return(
+        <div className="text-white hover:text-purple">
+            <button><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-arrow-big-left"><path d="M18 15h-6v4l-7-7 7-7v4h6v6z"/></svg></button>
+        </div>
+    )
+}
--- src/app/page/AboutMe/page.tsx
+++ src/app/page/AboutMe/page.tsx
@@ -1,19 +1,32 @@
+'use client'
+import {useRouter} from "next/navigation"
+import ButtonBack from "@/app/components/ButtonBack/page";
 
 export default function AboutMe() {
+
+    const router = useRouter();
+    const handleInput = () => {
+        router.push("http://localhost:3000")
+    }
+
     return(
-        <div className="flex align-item justify-center pt-72 gap-10">
-             <div>
-             <img src="../img/Avatar-Maker.png" alt="imagem cartoon" width="400" height="400"/>
+        <main className="">
+            <div className="" onClick={handleInput}>
+                <ButtonBack/>
             </div>
-            <div className="flex flex-col align-center justify-center gap-1">
-                <h1 className="text-4xl">Yago Santana Rocha</h1>
-                <p>Olá sou o yago Tenho 19 anos</p>
-                <p>Desenvolvedor Front-end</p>
-                <p>Analise e desenvolvimento de sistemas 4 Semestre</p>
-                <p>Interesses em ReactJS, NextJS, JavaScript</p>
+            <div className="flex items-center justify-center gap-8">
+                <div>
+                    <img src="../img/Avatar-Maker.png" alt="imagem cartoon" width="400" height="400"/>
+                </div>
+                <div className="flex flex-col align-center justify-center gap-1">
+                    <h1 className="text-4xl">Yago Santana Rocha</h1>
+                    <p>Olá sou o yago Tenho 19 anos</p>
+                    <p>Desenvolvedor Front-end</p>
+                    <p>Analise e desenvolvimento de sistemas 4 Semestre</p>
+                    <p>Interesses em ReactJS, NextJS, JavaScript</p>
+                </div>
             </div>
-           
-        </div>
+        </main>
         
     )
 }
