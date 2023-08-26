28e6e2f16e81505ee3d3572388dc3fc682aa4762
Yago <yagosr25@gmail.com>
2023-08-26 16:01:58 -0300

    adição de detalhes

--- src/app/page/ProjectsPage/page.tsx
+++ src/app/page/ProjectsPage/page.tsx
@@ -14,13 +14,13 @@ export default function ProjectsPage() {
             <div className="" onClick={handleInput}>
                 <ButtonBack/>
             </div>
-            <div className="flex justify-center items-center p-10 gap-5">
+            <div className="flex justify-center items-center gap-5">
                 <div className="flex flex-col items-center justify-center rounded-md border border-dark-purple w-64 h-64 gap-5 ">
                     <div className=" w-52 ">
                         <img src="../img/pokedex.png" alt="" />
                     </div>
-                    <div className="flex justify-center items-center text-sm">
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime nostrum repellendus vero persp</p>
+                    <div className="flex justify-center items-center text-xs w-44">
+                        <p>Pokedex com botões interativos onde se pode escolher qual pokemon visualizar</p>
                     </div>
                     <div className="flex justify-center gap-4 ">
                         <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm" >
@@ -33,33 +33,41 @@ export default function ProjectsPage() {
                 </div>
                 <div className="flex flex-col items-center justify-center rounded-md border border-dark-purple w-64 h-64 gap-5 ">
                     <div className=" w-52 ">
-                        <img src="../img/pokedex.png" alt="" />
+                        <img src="../img/spotify.png" alt="imagem projeto spotify" />
                     </div>
-                    <div className="flex justify-center items-center text-sm">
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime nostrum repellendus vero persp</p>
+                    <div className="flex justify-center items-center w-44 text-xs">
+                        <p>Tela estatica clone do aplicativo de musica Spotify</p>
                     </div>
                     <div className="flex justify-center gap-4 ">
-                        <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm" >
-                            Repositorios
+                        <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm">
+                            <a href="https://github.com/Yagosr/Spotify-clone">
+                                Repositorios
+                            </a>
                         </button>
-                        <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm"  >
-                            Visualizar
+                        <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm">
+                            <a href="https://silver-maamoul-9962ee.netlify.app/">
+                                Visualizar
+                            </a>
                         </button>
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-center rounded-md border border-dark-purple w-64 h-64 gap-5 ">
                     <div className=" w-52 ">
-                        <img src="../img/pokedex.png" alt="" />
+                        <img src="../img/netflix.png" alt="" />
                     </div>
-                    <div className="flex justify-center items-center text-sm">
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime nostrum repellendus vero persp</p>
+                    <div className="flex justify-center items-center text-xs w-44">
+                        <p>tela estatica clone da netflix, feita com HTML e CSS</p>
                     </div>
                     <div className="flex justify-center gap-4 ">
-                        <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm" >
-                            Repositorios
+                        <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm">
+                            <a href="https://github.com/Yagosr/Superflix">
+                                Repositorios
+                            </a>
                         </button>
-                        <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm"  >
-                            Visualizar
+                        <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm">
+                            <a href="https://silly-macaron-1f5be2.netlify.app/">
+                                Visualizar
+                            </a>
                         </button>
                     </div>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,30 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       },
       colors: {
         'dark-gray': '#090e16',
+        'gray':'#18263b',
         'purple': '#422c7c',
         'dark-purple':'#313866',
         'wine':'#352F44'
+      },
+      fontFamily: {
+        RobotoMono: ['Black Ops One', 'cursive']
       },
     },
   },
