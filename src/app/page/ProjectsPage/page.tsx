8107b5776113678832f5cb2c4dba93f535882f73
Yago <yagosr25@gmail.com>
2023-10-12 05:31:27 -0300

    novo projeto

--- src/app/page/ProjectsPage/page.tsx
+++ src/app/page/ProjectsPage/page.tsx
@@ -104,25 +104,28 @@ export default function ProjectsPage() {
                     </div>
 
                     <div className="flex flex-col items-center justify-center rounded-md border border-dark-purple w-64 h-64 gap-5 ">
-                    <h1>Em breve mais projetos</h1>
-                        {/* <div className=" w-52 ">
-                            <img src="../img/pokedex.png" alt="" />
+                        <div className=" w-52 ">
+                            <img src="../img/projetoReact.png" alt="" />
                         </div>
-                        <div className="flex justify-center items-center text-sm">
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime nostrum repellendus vero persp</p>
+                        <div className="flex justify-center items-center text-xs w-44">
+                            <p>Formulario onde o usuario atualiza seus dados desenvolvido com React.js</p>
                         </div>
                         <div className="flex justify-center gap-4 ">
-                            <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm" >
-                                Repositorios
-                            </button>
-                            <button className="border border-dark-purple bg-dark-purple h-10 w-24 hover:bg-dark-gray  text-sm"  >
-                                Visualizar
-                            </button>
-                        </div> */}
+                            <a href="https://github.com/Yagosr/React-study">
+                                <button className="border border-purple bg-purple h-10 w-24 hover:bg-dark-gray  text-sm" >
+                                    Repositorios
+                                </button>
+                            </a>
+                            <a href="https://monumental-figolla-b04b44.netlify.app/">
+                                <button className="border border-purple bg-purple h-10 w-24 hover:bg-dark-gray  text-sm"  >
+                                    Visualizar
+                                </button>
+                            </a>
+                        </div>
                     </div>
                     <div className="flex flex-col items-center justify-center rounded-md border border-dark-purple w-64 h-64 gap-5 ">
                         <div>
-                            <h1>Em breve mais projetos</h1>
+                            <h1>Em breve mais projetos.</h1>
                         </div>
                         {/* <div className=" w-52 ">
                             <img src="../img/pokedex.png" alt="" />
