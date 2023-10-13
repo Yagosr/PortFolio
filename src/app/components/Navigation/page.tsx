7dc374b1e2ff27088e659cb744216c9485a15d08
Yago <yagosr25@gmail.com>
2023-10-13 11:42:14 -0300

    ajustes rotas

--- src/app/components/Navigation/page.tsx
+++ src/app/components/Navigation/page.tsx
@@ -14,23 +14,23 @@ export  default function Navigation() {
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-home"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
                   Home
                 </Link>
-                <Link href="http://localhost:3000/page/AboutMe" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
+                <Link href="/AboutMe" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
                 About me
                 </Link>
-                <Link href="http://localhost:3000/page/Skills" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
+                <Link href="/Skills" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-dumbbell"><path d="m6.5 6.5 11 11"/><path d="m21 21-1-1"/><path d="m3 3 1 1"/><path d="m18 22 4-4"/><path d="m2 6 4-4"/><path d="m3 10 7-7"/><path d="m14 21 7-7"/></svg>
                   My Skills
                 </Link>
-                <Link href="http://localhost:3000/page/Carrer" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
+                <Link href="/Carrer" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-briefcase"><rect width="20" height="14" x="2" y="7" rx="2" ry="2"/><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"/></svg>
                   Career
                 </Link>
-                <Link href="http://localhost:3000/page/ProjectsPage" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
+                <Link href="/ProjectsPage" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-folder-git-2"><path d="M9 20H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h3.9a2 2 0 0 1 1.69.9l.81 1.2a2 2 0 0 0 1.67.9H20a2 2 0 0 1 2 2v5"/><circle cx="13" cy="12" r="2"/><path d="M18 19c-2.8 0-5-2.2-5-5v8"/><circle cx="20" cy="19" r="2"/></svg>
                   Projects
                 </Link>
-                <Link href="http://localhost:3000/page/Contact" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
+                <Link href="/Contact" className="flex gap-1 cursor-pointer text-zinc-200 hover:text-purple">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-contact-2"><path d="M16 18a4 4 0 0 0-8 0"/><circle cx="12" cy="11" r="3"/><rect width="18" height="18" x="3" y="4" rx="2"/><line x1="8" x2="8" y1="2" y2="4"/><line x1="16" x2="16" y1="2" y2="4"/></svg>
                   Contact
                 </Link>
